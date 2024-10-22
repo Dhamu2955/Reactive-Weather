5eb1c09c6451fa299f40a14612c34400c8c67230
Dhamu2955 <Dharmendra.Pindolia@gmail.com>
2024-10-22 14:41:58 +0100

    Part 2A done

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,12 +1,32 @@
 import React from "react";
-import sunny from "../assets/Sunny.svg"
+import Sunny from "../assets/Sunny.svg"
+import Cloudy from "../assets/Cloudy.svg"
+import Rainy from "../assets/Rainy.svg"
+import PartlyCloudy from "../assets/PartlyCloudy.svg"
 
 
 function WeatherCard({cities}) {
+  let image;
+  switch(cities.forecast){
+    case "Sunny" :
+      image = Sunny
+      break;
+    case "Cloudy" :
+      image = Cloudy
+      break; 
+    case "Rainy" :
+      image = Rainy
+      break;
+    case "Partly cloudy" :
+      image = PartlyCloudy
+      break; 
+
+  }
+
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src = {image} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
             <h3 className="card-title">{cities.city}</h3>
